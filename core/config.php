e8122ab8f564f66e9696f5cda6642b043f99893c
vtshark <vtshark@ukr.net>
2016-06-04 15:56:04 +0000

    04.06.2016

--- core/config.php
+++ core/config.php
@@ -7,7 +7,6 @@ define("DB_NAME", "social");
 define("DEBUG", true);
 define("SITE_TITLE", "Супер соц сеть site.com");
 
-//PATH
 define("ROOT", realpath("."));
 define("CONTROLLER_PATH", ROOT."/controllers/");
 define("TEMPLATE_PATH", ROOT."/template/");
